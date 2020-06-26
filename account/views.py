39eda1eba0c844549c575ea0472ab2d98ec5cd17
xw <1204327445@qq.com>
2020-06-26 20:23:03 +0800

    添加注册页面

--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    elif request.method == 'POST':
+        user_name = request.POST['用户名']
+        password1 = request.POST['密码']
+        password2 = request.POST['确认密码']
+        try:
+            User.objects.get(username=user_name)
+            return render(request, 'signup.html', {'用户名错误': '该用户名已存在'})
+        except User.DoesNotExist:
+            if password1 == password2:
+                User.objects.create_user(username=user_name, password=password1)
+                return redirect('主页')
+            else:
+                return render(request, 'signup.html', {'密码错误': '两次密码不同'})
--- ph/urls.py
+++ ph/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import products.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', products.views.procuct_list),
+    path('', products.views.procuct_list, name='主页'),
+    path('account/', include('account.urls')),
 ]
