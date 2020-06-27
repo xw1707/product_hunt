5dd548ad2b3ce38a6c452a3f0faa46a67e77083c
xw <1204327445@qq.com>
2020-06-27 16:18:21 +0800

    前端设计完成

--- ph/urls.py
+++ ph/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products.views.procuct_list, name='主页'),
     path('account/', include('account.urls')),
+    path('products/', include('products.urls')),
 ]
--- products/views.py
+++ products/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def procuct_list(request):
     return render(request, 'procuct_list.html')
+
+
+def publish(request):
+    if request.method == 'GET':
+        return render(request, 'publish.html')
+    elif request.method == 'POST':
+        app_name = request.POST['名称']
+        intro = request.POST['介绍']
+        url = request.POST['链接']
+        icon = request.POST['图标']
+        image = request.POST['大图']
+        return render(request, 'publish.html')
