e41b9ba906cdd0a181867e188be8b6b48841a7d3
xw <1204327445@qq.com>
2020-06-27 09:39:34 +0800

    登录和退出

--- account/urls.py
+++ account/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='注册页面'),
+    path('login/', views.login, name='登录页面'),
+    path('logout/', views.logout, name='退出页面'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 
@@ -16,7 +17,28 @@ def signup(request):
             return render(request, 'signup.html', {'用户名错误': '该用户名已存在'})
         except User.DoesNotExist:
             if password1 == password2:
-                User.objects.create_user(username=user_name, password=password1)
+                User.objects.create_user(
+                    username=user_name, password=password1)
                 return redirect('主页')
             else:
                 return render(request, 'signup.html', {'密码错误': '两次密码不同'})
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        user_name = request.POST['用户名']
+        pass_word = request.POST['密码']
+        user = auth.authenticate(username=user_name, password=pass_word)
+        if user is None:
+            return render(request, 'login.html', {'错误': '用户名或密码错误'})
+        else:
+            auth.login(request, user)
+            return redirect('主页')
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('主页')
