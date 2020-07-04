cd8a83cd8b9a0248f088908c848309dc7e8198f4
xw <1204327445@qq.com>
2020-07-04 17:00:26 +0800

    不知道改了些什么

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('publish/', views.publish, name='发布页面'),
-    path('<int:product_id)>', views.detail, name='产品细节'),
+    path('<int:product_id>', views.detail, name='产品细节'),
 ]
